03798e76b5e6b20bb5353b5e0acae09ca16a18c7
Sudqi Jawabreh <s.jawabreh@foothillsolutions.com>
2020-10-08 16:28:14 +0300

    impelemnt NumberToWord with unit tests

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using checks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestNumbersWithDecimals()
+        {
+            var util = new NumberToWordUtil();
+            var result = util.NumberToWord("1.241");
+            Assert.AreEqual("One And Two Hundred Forty One", result);
+
+            result = util.NumberToWord("10.7");
+            Assert.AreEqual("Ten And Seven Hundred", result);
+
+            result = util.NumberToWord("0.000");
+            Assert.AreEqual("", result);
+
+            result = util.NumberToWord("15.001");
+            Assert.AreEqual("Fifteen And One", result);
+
+            result = util.NumberToWord("31.01");
+            Assert.AreEqual("Thirty One And Ten", result);
+
+            result = util.NumberToWord("78.012");
+            Assert.AreEqual("Seventy Eight And Twelve", result);
+
+            result = util.NumberToWord("489.016");
+            Assert.AreEqual("Four Hundred Eighty Nine And Sixteen", result);
+
+            result = util.NumberToWord("1361.02");
+            Assert.AreEqual("One Thousand Three Hundred Sixty One And Twenty", result);
+
+            result = util.NumberToWord("15415.020");
+            Assert.AreEqual("Fifteen Thousand Four Hundred Fifteen And Twenty", result);
+
+            result = util.NumberToWord("21007.057");
+            Assert.AreEqual("Twenty One Thousand Seven And Fifty Seven", result);
+
+            result = util.NumberToWord("362789.157");
+            Assert.AreEqual("Three Hundred Sixty Two Thousand Seven Hundred Eighty Nine And One Hundred Fifty Seven", result);
+
+            result = util.NumberToWord("1452710.907");
+            Assert.AreEqual("One Million Four Hundred Fifty Two Thousand Seven Hundred Ten And Nine Hundred Seven", result);
+        }
+
+        [TestMethod]
+        public void TestDecimals()
+        {
+            var util = new NumberToWordUtil();
+            var result = util.NumberToWord("0.241");
+            Assert.AreEqual("Two Hundred Forty One", result);
+
+            result = util.NumberToWord("0.7");
+            Assert.AreEqual("Seven Hundred", result);
+
+            result = util.NumberToWord("0.000");
+            Assert.AreEqual("", result);
+
+            result = util.NumberToWord("0.001");
+            Assert.AreEqual("One", result);
+
+            result = util.NumberToWord("0.01");
+            Assert.AreEqual("Ten", result);
+
+            result = util.NumberToWord("0.012");
+            Assert.AreEqual("Twelve", result);
+
+            result = util.NumberToWord("0.016");
+            Assert.AreEqual("Sixteen", result);
+
+            result = util.NumberToWord("0.02");
+            Assert.AreEqual("Twenty", result);
+
+            result = util.NumberToWord("0.020");
+            Assert.AreEqual("Twenty", result);
+
+            result = util.NumberToWord("0.057");
+            Assert.AreEqual("Fifty Seven", result);
+
+            result = util.NumberToWord("0.157");
+            Assert.AreEqual("One Hundred Fifty Seven", result);
+
+            result = util.NumberToWord("0.907");
+            Assert.AreEqual("Nine Hundred Seven", result);
+
+            result = util.NumberToWord("0.400");
+            Assert.AreEqual("Four Hundred", result);
+
+            result = util.NumberToWord("0.40");
+            Assert.AreEqual("Four Hundred", result);
+
+            result = util.NumberToWord("0.4");
+            Assert.AreEqual("Four Hundred", result);
+        }
+
+        [TestMethod]
+        public void TestDigits()
+        {
+            var util = new NumberToWordUtil();
+            var result = util.NumberToWord("9");
+            Assert.AreEqual("Nine", result);
+
+            result = util.NumberToWord("2");
+            Assert.AreEqual("Two", result);
+
+            result = util.NumberToWord("1.0");
+            Assert.AreEqual("One", result);
+
+            result = util.NumberToWord("7.0");
+            Assert.AreEqual("Seven", result);
+
+            result = util.NumberToWord("8");
+            Assert.AreEqual("Eight", result);
+
+            result = util.NumberToWord("6");
+            Assert.AreEqual("Six", result);
+
+            result = util.NumberToWord("3");
+            Assert.AreEqual("Three", result);
+
+            result = util.NumberToWord("4");
+            Assert.AreEqual("Four", result);
+
+            result = util.NumberToWord("0");
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void TestTens()
+        {
+            var util = new NumberToWordUtil();
+            var result = util.NumberToWord("10.0");
+            Assert.AreEqual("Ten", result);
+
+            result = util.NumberToWord("15");
+            Assert.AreEqual("Fifteen", result);
+
+            result = util.NumberToWord("25");
+            Assert.AreEqual("Twenty Five", result);
+
+            result = util.NumberToWord("30");
+            Assert.AreEqual("Thirty", result);
+
+            result = util.NumberToWord("78");
+            Assert.AreEqual("Seventy Eight", result);
+
+            result = util.NumberToWord("41");
+            Assert.AreEqual("Forty One", result);
+
+            result = util.NumberToWord("90.0");
+            Assert.AreEqual("Ninety", result);
+
+            result = util.NumberToWord("900.0");
+            Assert.AreEqual("Nine Hundred", result);
+
+            result = util.NumberToWord("950.0");
+            Assert.AreEqual("Nine Hundred Fifty", result);
+
+            result = util.NumberToWord("159.0");
+            Assert.AreEqual("One Hundred Fifty Nine", result);
+        }
+
+        [TestMethod]
+        public void TestHundreds()
+        {
+            var util = new NumberToWordUtil();
+            var result = util.NumberToWord("100.0");
+            Assert.AreEqual("One Hundred", result);
+
+            result = util.NumberToWord("200.0");
+            Assert.AreEqual("Two Hundred", result);
+
+            result = util.NumberToWord("900.0");
+            Assert.AreEqual("Nine Hundred", result);
+
+            result = util.NumberToWord("950.0");
+            Assert.AreEqual("Nine Hundred Fifty", result);
+
+            result = util.NumberToWord("159.0");
+            Assert.AreEqual("One Hundred Fifty Nine", result);
+        }
+
+        [TestMethod]
+        public void TestThousands()
+        {
+            var util = new NumberToWordUtil();
+            var result = util.NumberToWord("1000.0");
+            Assert.AreEqual("One Thousand", result);
+
+            result = util.NumberToWord("2000.0");
+            Assert.AreEqual("Two Thousand", result);
+
+            result = util.NumberToWord("9000.0");
+            Assert.AreEqual("Nine Thousand", result);
+
+            result = util.NumberToWord("9675.0");
+            Assert.AreEqual("Nine Thousand Six Hundred Seventy Five", result);
+
+            result = util.NumberToWord("9075.0");
+            Assert.AreEqual("Nine Thousand Seventy Five", result);
+
+            result = util.NumberToWord("9005.0");
+            Assert.AreEqual("Nine Thousand Five", result);
+
+            result = util.NumberToWord("9011.0");
+            Assert.AreEqual("Nine Thousand Eleven", result);
+        }
+    }
+}
---
+++ checks/NumberToWordUtil.cs
@@ -0,0 +1,255 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace checks
+{
+    static class Extensions
+    {
+        public static string SpaceFormat(this string value)
+        {
+            return (string.IsNullOrEmpty(value) ? string.Empty : " ") + value;
+        }
+    }
+    public class NumberToWordUtil
+    {
+        public string NumberToWord(string value)
+        {
+            double.TryParse(value, out var number);
+            var periodIndex = value.IndexOf(".");
+            if (periodIndex >= 0)
+            {
+                var first = value.Substring(0, periodIndex);
+                var second = (periodIndex < value.Length - 1) ? value.Substring(periodIndex + 1) : string.Empty;
+                first = PadLeft(first);
+                second = PadRight(second);
+                var words = GetWords(first);
+                var decimals = string.Join(" ",GetHundreds(second.ToArray().ToList()).Where(c => !string.IsNullOrEmpty(c)));
+                if (!string.IsNullOrEmpty(words) && !string.IsNullOrEmpty(decimals))
+                {
+                    return words + " And " + decimals;
+                }
+                else if(!string.IsNullOrEmpty(words))
+                {
+                    return words;
+                }
+                else if(!string.IsNullOrEmpty(decimals))
+                {
+                    return decimals;
+                }
+            }
+            else
+            {
+                var first = value;
+                first = PadLeft(first);
+                var words = GetWords(first);
+                return words;
+            }
+
+            return string.Empty;
+        }
+        public string AmountInJDToWords(string value)
+        {
+            return CurrencyToWords(value, "JD", "Fils", "Only");
+        }
+        public string CurrencyToWords(string value, string prefix , string decimalSuffix, string suffix)
+        {            
+            double.TryParse(value, out var number);
+            var periodIndex = value.IndexOf(".");
+            if (periodIndex >= 0)
+            {
+                var first = value.Substring(0, periodIndex);
+                var second = (periodIndex < value.Length - 1) ? value.Substring(periodIndex + 1) : string.Empty;
+                first = PadLeft(first);
+                second = PadRight(second);
+                var words = GetWords(first);
+                var decimals = string.Join(" ",GetHundreds(second.ToArray().ToList()).Where(c => !string.IsNullOrEmpty(c)));
+                if (!string.IsNullOrEmpty(words) && !string.IsNullOrEmpty(decimals))
+                {
+                    return prefix + " " + words + " And " + decimals + " " + decimalSuffix + " " + suffix;
+                }
+                else if(!string.IsNullOrEmpty(words))
+                {
+                    return prefix + " " + words + " And No " + decimalSuffix + " " + suffix;
+                }
+                else if(!string.IsNullOrEmpty(decimals))
+                {
+                    return prefix + " " + decimals + " " + decimalSuffix + " " + suffix;
+                }
+            }
+            else
+            {
+                var first = value;
+                first = PadLeft(first);
+                var words = GetWords(first);
+                return words;
+            }
+
+            return string.Empty;
+
+
+        }
+        private string GetWords(string first)
+        {
+            var groupOfThrees = first.ToArray()
+                .Select((e, i) => new { e, i })
+                .GroupBy(x => x.i / 3)
+                .Select((x, i) => new { i, e = x.Select(y => y.e).ToList() });
+
+            return string.Join(" ", groupOfThrees.Select(g =>
+            {
+                var hundred = GetHundreds(g.e);
+                var order = GetOrder(g.i);
+                if (hundred.Where(x => !string.IsNullOrEmpty(x)).Any())
+                    hundred.Add(order);
+                return hundred;
+            }).SelectMany(g => g).Where(g => !string.IsNullOrEmpty(g)));
+        }
+
+        private string GetOrder(int i)
+        {
+            switch (i)
+            {
+                case 0:
+                    return "Million";
+                case 1:
+                    return "Thousand";
+                case 2:
+                    return string.Empty;
+                default:
+                    return string.Empty;
+            }
+        }
+
+        private List<string> GetHundreds(List<char> e)
+        {
+            var hundred = e[0];
+            var ten = e[1];
+            var digit = e[2];
+            var value = GetDigtit(hundred);
+            var value1 = Rest(e);
+
+            var result = new List<string> { value };
+            if (!string.IsNullOrEmpty(value))
+            {
+                result.Add("Hundred");
+            }
+            result.AddRange(value1);
+            return result;
+        }
+
+        private List<string> Rest(List<char> e)
+        {
+            var ten = e[1];
+            var digit = e[2];
+            if(ten == '0' && digit == '0')
+            {
+                return new List<string>();
+            }
+            else if (ten != '1')
+            {
+                return new List<string> { GetTen(ten), GetDigtit(digit) };
+            }
+            else
+            {
+                return new List<string> { GetTeens($"{ten}{digit}") };
+            }
+
+        }
+
+        private string GetTeens(string v)
+        {
+            switch (v)
+            {
+                case "10":
+                    return "Ten";
+                case "11":
+                    return "Eleven";
+                case "12":
+                    return "Twelve";
+                case "13":
+                    return "Thirteen";
+                case "14":
+                    return "Fourteen";
+                case "15":
+                    return "Fifteen";
+                case "16":
+                    return "Sixteen";
+                case "17":
+                    return "Seventeen";
+                case "18":
+                    return "Eighteen";
+                case "19":
+                    return "Nineteen";
+            }
+
+            return string.Empty;
+        }
+
+        private string GetTen(char ten)
+        {
+            switch (ten)
+            {
+                case '2':
+                    return "Twenty";
+                case '3':
+                    return "Thirty";
+                case '4':
+                    return "Forty";
+                case '5':
+                    return "Fifty";
+                case '6':
+                    return "Sixty";
+                case '7':
+                    return "Seventy";
+                case '8':
+                    return "Eighty";
+                case '9':
+                    return "Ninety";
+            }
+
+            return string.Empty;
+        }
+
+        private string GetDigtit(char hundred)
+        {
+            switch (hundred)
+            {
+                case '1':
+                    return "One";
+                case '2':
+                    return "Two";
+                case '3':
+                    return "Three";
+                case '4':
+                    return "Four";
+                case '5':
+                    return "Five";
+                case '6':
+                    return "Six";
+                case '7':
+                    return "Seven";
+                case '8':
+                    return "Eight";
+                case '9':
+                    return "Nine";
+            }
+
+            return string.Empty;
+        }
+
+        private string PadLeft(string value)
+        {
+            var numberOfPadding = (9 - value.Length);
+            return string.Join("", Enumerable.Range(0, numberOfPadding).Select(i => "0")) + value;
+        }
+
+        private string PadRight(string value)
+        {
+            var numberOfPadding = (3 - value.Length);
+            return value + string.Join("", Enumerable.Range(0, numberOfPadding).Select(i => "0"));
+        }
+    }
+
+}
