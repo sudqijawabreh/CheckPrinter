b11c1c4b2992b236a9ae2d8b81f706d85a5387e3
Sudqi Jawabreh <s.jawabreh@foothillsolutions.com>
2021-01-07 20:16:24 +0200

    Add DatePrompt

---
+++ checks/Prompts/DatePrompt.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace checks
+{
+    partial class DatePrompt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OK = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(12, 61);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(82, 30);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.Location = new System.Drawing.Point(147, 61);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(85, 30);
+            this.Cancel.TabIndex = 2;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(62, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose Check Date";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CustomFormat = "dddd dd/MM/yyy";
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(38, 35);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dateTimePicker.Size = new System.Drawing.Size(157, 20);
+            this.dateTimePicker.TabIndex = 4;
+            // 
+            // DatePrompt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 101);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.OK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DatePrompt";
+            this.Text = "Choose A Date";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+    }
+}
---
+++ checks/Prompts/DatePrompt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace checks
+{
+    public partial class DatePrompt : Form
+    {
+        public static PromptEventArgs ShowPrompt(List<string> items)
+        {
+            var p = new DatePrompt(items);
+            p.StartPosition = FormStartPosition.CenterParent;
+            p.AcceptButton = p.OK;
+            var dialogResult = p.ShowDialog();
+            return new PromptEventArgs
+            {
+                ChoiceType = dialogResult == DialogResult.OK ? PromptChoice.OK : PromptChoice.Cancel,
+                Item = p.dateTimePicker.Value.ToString() ?? DateTime.Now.ToString(),
+            };
+        }
+        public DatePrompt(List<string> sheets)
+        {
+            InitializeComponent();
+            OK.DialogResult = DialogResult.OK;
+            Cancel.DialogResult = DialogResult.Cancel;
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
