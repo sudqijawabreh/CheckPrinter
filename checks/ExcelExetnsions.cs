bd2f88073302b799601bbaa61eeea89873531a6f
Sudqi Jawabreh <s.jawabreh@foothillsolutions.com>
2021-01-24 12:07:57 +0200

    Add formating to history export file

---
+++ checks/ExcelExetnsions.cs
@@ -0,0 +1,23 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checks
+{
+    public static class ExcelExetnsions
+    {
+        public static IXLCell ApplyCell(this IXLCell cell, Action<IXLCell> action)
+        {
+            action(cell);
+            return cell;
+        }
+        public static IEnumerable<IXLCell> GetRowCells(this IXLRow row, int columns)
+        {
+            return Enumerable.Range(1, columns).Select(i => row.Cell(i));
+        }
+    }
+}
--- checks/ReportGenerator.cs
+++ checks/ReportGenerator.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace checks
 {
-    class ReportGenerator
+    partial class ReportGenerator
     {
         public static void Genereate(string importedFileName, string exportFileName, string sheetName, int headerRow, List<string> SN)
         {
@@ -43,21 +43,51 @@ namespace checks
             ws.Cell(1, 6).Value = "Amount";
             ws.Cell(1, 7).Value = "Check Date";
             ws.Cell(1, 8).Value = "Print Date";
-            records.Select((r, i) => new { record = r, index = i }).ToList().ForEach(v =>
+            ws.Cell(1, 9).Value = "Area";
+            records.Select((r, i) => new { record = r, index = i + 1 }).ToList().ForEach(v =>
               {
-                  ws.Cell(v.index + 2, 1).Value = (v.index + 1).ToString();
-                  ws.Cell(v.index + 2, 2).Value = v.record.SN;
-                  ws.Cell(v.index + 2, 3).Value = v.record.IDNumber;
-                  ws.Cell(v.index + 2, 4).Value = v.record.Name;
-                  ws.Cell(v.index + 2, 5).Value = v.record.Currency;
-                  ws.Cell(v.index + 2, 6).Value = v.record.Amount;
-                  ws.Cell(v.index + 2, 7).Value = v.record.CheckDate;
-                  ws.Cell(v.index + 2, 8).Value = v.record.PrintDate;
+                  ws.Cell(v.index + 1, 1).Value = v.index.ToString();
+                  ws.Cell(v.index + 1, 2).Value = v.record.SN;
+                  ws.Cell(v.index + 1, 3).Value = v.record.IDNumber;
+                  ws.Cell(v.index + 1, 4).Value = v.record.Name;
+                  ws.Cell(v.index + 1, 5).Value = v.record.Currency;
+                  ws.Cell(v.index + 1, 6).Value = v.record.Amount;
+                  ws.Cell(v.index + 1, 7).Value = v.record.CheckDate;
+                  ws.Cell(v.index + 1, 8).Value = v.record.PrintDate;
+                  ws.Cell(v.index + 1, 9).Value = v.record.Area;
               });
-            Enumerable.Range(1, 8).ToList().ForEach(i =>
+            Enumerable.Range(1, 9).ToList().ForEach(i =>
              {
                  ws.Column(i).AdjustToContents();
+                 ws.Column(i).Width = 17;
              });
+
+            ws.Row(1).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            ws.Row(1).Style.Alignment.SetVertical(XLAlignmentVerticalValues.Center);
+            ws.Row(1).Style.Font.FontSize = 12;
+            ws.Row(1).Style.Font.Bold = true;
+
+            ws.Rows(1, records.Count + 1).ToList().ForEach(row =>
+             {
+                 row.GetRowCells(9).ToList().ForEach(c =>
+                {
+                       c.Style.Border.SetBottomBorder(XLBorderStyleValues.Thin);
+                       c.Style.Border.SetLeftBorder(XLBorderStyleValues.Thin);
+                       c.Style.Border.SetTopBorder(XLBorderStyleValues.Thin);
+                       c.Style.Border.SetRightBorder(XLBorderStyleValues.Thin);
+
+                       c.Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+                       c.Style.Alignment.SetVertical(XLAlignmentVerticalValues.Center);
+                });
+            });
+
+            ws.Row(1).Height = 35;
+            ws.Rows(2, records.Count() + 1).ToList().ForEach(row =>
+             {
+                 row.Height = 25;
+
+             });
+            ws.Column(4).Width = 41;
 
             workbook.SaveAs(exportFileName);
         }
