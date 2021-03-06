ff2e573984e70b57dec8f84b6840e8ecd7373a20
Sudqi Jawabreh <s.jawabreh@foothillsolutions.com>
2021-03-06 20:59:17 +0200

    Add full check image as a resource

--- checks/Properties/Resources.Designer.cs
+++ checks/Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace checks.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap full_check {
+            get {
+                object obj = ResourceManager.GetObject("full_check", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap history_pngrepo_com {
             get {
                 object obj = ResourceManager.GetObject("history-pngrepo-com", resourceCulture);
